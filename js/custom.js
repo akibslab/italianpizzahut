6c61305970505e9d10d5de0794f3f8ae00355ebb
Akib Hossain <akib.hossain.iub@gmail.com>
2021-12-14 12:57:35 +0600

    Mobile responsive done

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,60 @@
 (function($) {
     "use strict";
+
+    let headerHeight = $('.top-navbar .navbar').outerHeight();
+
+    // Select all links with hashes
+      $('.smooth-scroll')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - headerHeight
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+
+
+      $('#menu-section .menu-tabs-button ul li a').on('click', function(e){
+      	e.preventDefault();
+      	$('#menu-section .menu-tabs-button ul li a').removeClass('active');
+      	$(this).addClass('active');
+
+      	console.log($(this).attr('data-index'));
+      	if($(this).attr('data-index') == '*') {
+      		$('#menu-section .menu-tab').show();
+      	} else {
+      		$('#menu-section .menu-tab').hide();
+      		$('#menu-section #menu-tab-' + $(this).attr('data-index')).show();
+      	}
+      })
+
 	
 	/* ..............................................
 	Loader 
@@ -83,14 +138,7 @@
 	});
 	
 	
-	
-	/* ..............................................
-    Datepicker
-    ................................................. */
-	
-	$('.datepicker').pickadate();
-	
-	$('.time').pickatime();
+
 	
 	
 	
